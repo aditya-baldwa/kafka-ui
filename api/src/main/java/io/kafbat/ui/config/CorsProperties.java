9c6a8e1424a7de520040bff3dfd029565cc68d36
Aditya Baldwa <adityabaldwa767@gmail.com>
2024-10-19 14:04:12 +0530

    Correct checkstyle

--- api/src/main/java/io/kafbat/ui/config/CorsProperties.java
+++ api/src/main/java/io/kafbat/ui/config/CorsProperties.java
@@ -4,9 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import lombok.Data;
 
+@Data
 @Component
 @ConfigurationProperties(prefix = "cors")
-@Data
 
 public class CorsProperties {
 
